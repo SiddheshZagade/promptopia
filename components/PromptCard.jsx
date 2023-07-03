8c85b4614c086959539872422569aa9b9e940d06
SiddheshZagade <siddeshzagade7@gmail.com>
2023-07-03 16:42:07 +0530

    prompt display

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,11 +1,21 @@
+'use client';
 import React from 'react'
+import { useState } from 'react';
+import Image from 'next/image';
+import {useSession,useRouter} from 'next/navigation';
 
-const PromptCard = () => {
+const PromptCard = (post,handleTagClck,handleEdit,handleDelete) => {
   return (
-    <div>
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
       
+      
+          
+      </div>
     </div>
+    
+    
   )
 }
 
-export default PromptCard
+export default PromptCard;
